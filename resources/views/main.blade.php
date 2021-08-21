1bc0732af3679b30bcb2287efd8df193be15eb09
mosaddekmolla <mosaddek6@gmail.com>
2021-08-21 16:51:38 +0600

    changes on main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,7 @@
 
 
     <h2>Welcome to CV Builder</h2>
+    <h6>Make Your CV Online</h6>
 
     <a name="" id="" class="btn btn-primary" href=" {{route('user-detail.create')}} " role="button">Build Now</a>
 
